9500dc51fc064e865cbd82d21118b3ac3b9d387e
chacka1315 <chacka131@gmail.com>
2025-11-11 17:16:11 +0000

    Setup for the prisma ORM. And write a basic version of app.js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
+import 'dotenv/config';
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
-import 'dotenv/config';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import morgan from 'morgan';
 import NotFoundError from './errors/NotFoundError.js';
 import configurePassport from './config/passport.js';
+import prisma from './prisma/client.js';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const assetsPath = path.join(__dirname, 'public');
+const sessionStore = new PrismaSessionStore(prisma, {
+  dbRecordIdIsSessionId: true,
+  checkPeriod: 1000 * 60 * 60 * 24, // 1 day
+});
 
 //app setup
 const app = express();
@@ -21,6 +27,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+  }),
+);
+configurePassport(passport);
+app.use(passport.session());
+app.use((req, res, next) => {
+  if (req.user) res.locals.user = req.user;
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
 
 //errors handling
 app.use((req, res) => {
