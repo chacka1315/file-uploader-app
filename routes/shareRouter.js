6fadea36725d1bcc0cdc8f332897d9a318671625
chacka1315 <chacka131@gmail.com>
2025-11-16 13:38:33 +0000

    Finish writing the share router.

--- routes/shareRouter.js
+++ routes/shareRouter.js
@@ -1,6 +1,12 @@
 import express from 'express';
-import prisma from '../prisma/client.js';
+
+import shareController from '../controllers/shareController.js';
 
 const shareRouter = express.Router();
+
+shareRouter.get('/create/:folderId', shareController.create_share_get);
+shareRouter.post('/create/:folderId', shareController.create_share_post);
+shareRouter.get('/:shareId', shareController.read_share_get);
+shareRouter.get('/link/:folderId', shareController.viewSharelink_get);
 
 export default shareRouter;
